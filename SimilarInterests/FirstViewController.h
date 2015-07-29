e11472d1d6d87687826adff7b9c933e3b8da961e
Healthhub <healthhub@lawn-128-61-75-239.lawn.gatech.edu>
2015-07-29 14:24:20 -0400

    Initial Commit

---
+++ SimilarInterests/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  SimilarInterests
+//
+//  Created by Healthhub on 7/29/15.
+//  Copyright (c) 2015 Healthhub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ SimilarInterests/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  SimilarInterests
+//
+//  Created by Healthhub on 7/29/15.
+//  Copyright (c) 2015 Healthhub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
